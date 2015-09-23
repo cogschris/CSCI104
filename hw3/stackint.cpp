55365409499ec2efaad52de5cf5128c0f8306a27
Chris Cognetta <cognettachris@gmail.com>
2015-09-22 17:45:02 -0700

    Finished problem 3

--- hw3/stackint.cpp
+++ hw3/stackint.cpp
@@ -1,31 +1,55 @@
+#include <iostream>
 #include "stackint.h"
+#include <cstdlib>
+#include <stdexcept>
+using namespace std;
 
 StackInt::StackInt()
 {
-	list_.AListInt();
+	
 }
 
 StackInt::~StackInt()
 {
-	list_.clear();
+	
 }
 
-bool StackInt::empty() const;
+bool StackInt::empty() const
 {
 	return list_.empty();
 }
 
 void StackInt::push(const int& val)
 {
-	list_.insert(0, val);
+	list_.insert((list_.size()), val);
 }
 
 int const & StackInt::top() const
 {
-	list_.get(0);
+	return list_.get((list_.size() - 1));
 }
 
 void StackInt::pop()
 {
-	list_.remove(0);
+	list_.remove((list_.size() - 1));
 }
+
+/*
+int main() {
+	StackInt test;
+
+	test.push(5);
+	test.push(4);
+	test.push(3);
+
+	cout << test.top() << endl;
+	test.pop();
+	cout << test.top() << endl;
+	test.pop();
+	cout << test.top() << endl;
+	test.pop();
+	cout << test.empty() << endl;
+
+	return 0;
+}
+*/
