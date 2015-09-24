c4659ec44db282051fc6baf01224c7bd326113ad
Chris Cognetta <cognettachris@gmail.com>
2015-09-23 21:51:20 -0700

    Finished problem 5 and 4 and now just commenting

--- hw3/problem5.cpp
+++ hw3/problem5.cpp
@@ -1,3 +1,11 @@
+/*
+NOTE:
+I made up my own values for each symbol
+True values: -1
+False Values: -2
+"(" : -3
+
+*/
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -37,23 +45,28 @@ int main(int argc, char* argv[]) {
 	ifstream input;
 	input.open(argv[1]);
 
-	StackInt eval;
+	
 	string temp;
 	string number;
 	int parencounter = 0;
 	//int numcounter = 0;
 	bool failed = false;
 	int size = 0;
-	
+	int negative = 0;
+	bool failworld = false;
 
 	while (!input.eof()) {
 		getline(input, temp);
 		size = temp.size();
+		StackInt eval;
 		failed = false;
+		failworld = false;
+		parencounter = 0;
+		number.clear();
 
 		for (int i = 0; i < size; i++) {
 
-			if (failed == true) {
+			if (failed == true || failworld == true) {
 				break;
 			}
 
@@ -65,13 +78,31 @@ int main(int argc, char* argv[]) {
 
 						if (key[number] == true) {
 
-							eval.push(-1);
+							if (negative == 1) {
+								eval.pop();
+								eval.push(-2);
+							}
+							else if(negative == 0) {
+								eval.push(-1);
+							}
 						}
 
 						else if (key[number] == false) {
-
-							eval.push(-2);
+							if (negative == 1) {
+								eval.pop();
+								eval.push(-1);
+							}
+							else if (negative == 0) {
+								eval.push(-2);
+							}
 						}
+						negative = 0;
+					}
+
+					else {
+
+						failworld = true;
+						break;
 					}
 					number.clear();
 					//numcounter = 0;
@@ -80,18 +111,22 @@ int main(int argc, char* argv[]) {
 				if (temp[i] == '(') {
 					eval.push(-3);
 					parencounter++;
+					negative = 0;
 				}
 
 				if (temp[i] == '&') {
 					eval.push(-4);
+					negative = 0;
 				}
 
 				if (temp[i] == '|') {
 					eval.push(-5);
+					negative = 0;
 				}
 
 				if (temp[i] == '~') {
 					eval.push(-6);
+					negative = 1;
 				}
 
 			}
@@ -118,10 +153,15 @@ int main(int argc, char* argv[]) {
 							eval.push(-2);
 						}
 					}
+					else {
+						failworld = true;
+						break;
+					}
 					number.clear();
 					//numcounter = 0;
 				}
-
+				int andcheck = 0;
+				int orcheck = 0;
 				while (eval.top() != (-3)) {
 
 					if (eval.empty() == true) {
@@ -137,20 +177,31 @@ int main(int argc, char* argv[]) {
 						break;
 					}
 					int symbol = eval.top();
+					if (symbol == -4) {
+						andcheck = 1;
+					}
+					if (symbol == -5) {
+						orcheck = 1;
+					}
 					eval.pop();
+
 					int second = eval.top();
 
-					if ((first <= -3) || (second <= -4)) {
-						//cout << "Malformed" << endl;
-						failed = true;
-					}
+						
 					
 					if (symbol != (-6)) {
-						
+						if ((first <= -3) || (second <= -4)) {
+							//cout << "Malformed" << endl;
+							failed = true;
+						}
 						eval.pop();
 
+						if (andcheck == 1 && orcheck == 1) {
+							failed = true;
+						}
+
 						if (symbol == -4) {
-							if (first == second) {
+							if (first == -1 && second == -1) {
 								eval.push(-1);
 							}
 							else {
@@ -172,7 +223,7 @@ int main(int argc, char* argv[]) {
 						if (first == -1) {
 							eval.push(-2);
 						}
-						else if (second == -2) {
+						else if (first == -2) {
 							eval.push(-1);
 						}
 					}
@@ -188,36 +239,93 @@ int main(int argc, char* argv[]) {
 
 		if (failed == false) {
 			while (eval.empty() == false) {
+				if (number[0] != '\0') {
+
+					if (key.find(number) != key.end()) {
+
+						if (key[number] == true) {
+
+							if (negative == 1) {
+								eval.pop();
+								eval.push(-2);
+							}
+							else if(negative == 0) {
+								eval.push(-1);
+							}
+						}
+
+						else if (key[number] == false) {
+							if (negative == 1) {
+								eval.pop();
+								eval.push(-1);
+							}
+							else if (negative == 0) {
+								eval.push(-2);
+							}
+						}
+						negative = 0;
+					}
+
+					else {
+
+						failworld = true;
+						break;
+					}
+					number.clear();
+					//numcounter = 0;
+				}
 				int first = eval.top();
 				eval.pop();
 				int symbol = eval.top();
 				eval.pop();
 				int second = eval.top();
 
-				if ((first <= -3) || (second < -3)) {
-					cout << "Malformed" << endl;
+				if ((first <= -3) ) {
+					//cout << "Malformed" << endl;
 					failed = true;
 				}
 						
 				if (symbol != (-6)) {
 					
 					eval.pop();
+					if (symbol == -4) {
+							if (first == -1 && second == -1) {
+								eval.push(-1);
+							}
+							else {
+								eval.push(-2);
+							}
+						}
+
+						else if (symbol == -5) {
+							if ((first == -1) || (second == -1)) {
+								eval.push(-1);
+							}
+							else {
+								eval.push(-2);
+							}
+						}
 				}
 
 				else if (symbol == (-6)) {
 					if (first == -1) {
 						eval.push(-2);
 					}
-					else if (second == -2) {
+					else if (first == -2) {
 						eval.push(-1);
 					}
 				}
 			}
 		}
 
-		if (failed == true || parencounter > 0) {
+		if (failworld == true) {
+			cout << "Unknown variable" << endl;
+		}
+		else if (failed == true || parencounter > 0) {
 			cout << "Malformed" << endl;
 		}
+
+		
 		else if ((eval.top() == -1) && (failed != true)) {
 			cout << "True" << endl;
 		}
@@ -225,9 +333,7 @@ int main(int argc, char* argv[]) {
 			cout << "False" << endl;
 		}
 
-		while (eval.empty() == false) {
-			eval.pop();
-		}
+		
 
 	}
 	input.close();
